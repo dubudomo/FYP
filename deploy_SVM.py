08d4ceb8d008cd13338fa8bf9e77cbe695a26bd7
dubudomo <76898791+dubudomo@users.noreply.github.com>
2021-03-06 18:46:05 +0800

    Update deploy_SVM.py

--- deploy_SVM.py
+++ deploy_SVM.py
@@ -2,7 +2,6 @@ import pickle
 import streamlit as st
 import pandas as pd
 def predict(sentence):
-    # loading
     
     pred_res=[]
     
@@ -11,7 +10,7 @@ def predict(sentence):
     with open('model1.pickle', 'rb') as handle:
         model1 = pickle.load(handle)
     pred1 = model1.predict(vectorizer1.transform([sentence]))
-#     print(pred1)
+
     pred_res.append(pred1)
 
     with open('vectorizer2.pickle', 'rb') as handle:
@@ -19,7 +18,7 @@ def predict(sentence):
     with open('model2.pickle', 'rb') as handle:
         model2 = pickle.load(handle)
     pred2 = model2.predict(vectorizer2.transform([sentence]))
-#     print(pred2)
+
     pred_res.append(pred2)
     
     with open('vectorizer3.pickle', 'rb') as handle:
@@ -27,7 +26,7 @@ def predict(sentence):
     with open('model3.pickle', 'rb') as handle:
         model3 = pickle.load(handle)
     pred3 = model3.predict(vectorizer3.transform([sentence]))
-#     print(pred3)
+
     pred_res.append(pred3)
     
     with open('vectorizer4.pickle', 'rb') as handle:
@@ -35,7 +34,7 @@ def predict(sentence):
     with open('model4.pickle', 'rb') as handle:
         model4 = pickle.load(handle)
     pred4 = model4.predict(vectorizer4.transform([sentence]))
-#     print(pred4)
+
     pred_res.append(pred4)
     
     with open('vectorizer5.pickle', 'rb') as handle:
@@ -43,7 +42,7 @@ def predict(sentence):
     with open('model5.pickle', 'rb') as handle:
         model5 = pickle.load(handle)
     pred5 = model5.predict(vectorizer5.transform([sentence]))
-#     print(pred5.astype(int))
+
     pred_res.append(pred5)
     pred_df = pd.DataFrame(pred_res, columns=['sentiment'])
     for i in range(len(pred_df)):
@@ -53,15 +52,13 @@ def predict(sentence):
             pred_df['sentiment'].iloc[i] = "neutral"
         else:
             pred_df['sentiment'].iloc[i] = "negative"
-            
-
     return pred_df
 
 
 st.write("Please input a sentence here")
 user_input = st.text_area("Hit CTRL+Enter to run", "This is an amazing product")
 df = predict(user_input)
-
+st.title("Sentiment Analysis in Product Review")
 st.write("The final output sentiment")
 st.dataframe(df['sentiment'].value_counts(normalize=True).mul(100).round(1).astype(str) + '%'
 )
